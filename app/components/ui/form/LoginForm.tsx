0458af49b893ec95d4eed89b9c9c0c6bc020db1b
MohamadHosein86-Dev <mohamadhoseinsafaryan86@gmail.com>
2025-07-24 14:57:51 +0330

    Edite Css HomePage Titel

--- app/components/ui/form/LoginForm.tsx
+++ app/components/ui/form/LoginForm.tsx
@@ -2,7 +2,6 @@
 
 import { FormState } from "@/app/types/DataTypes";
 
-
 interface LoginFormProps {
   form: Pick<FormState, "email" | "password">;
   setForm: React.Dispatch<React.SetStateAction<FormState>>;
@@ -13,9 +12,9 @@ export default function LoginForm({ form, setForm, handleLogin }: LoginFormProps
   return (
     <form onSubmit={handleLogin} className="max-w-sm mx-auto mt-12 flex flex-col gap-4">
       <label htmlFor="">email</label>
-      <input type="email" className=" border-1 border-primary-border rounded-xl px-3 py-3" placeholder="Email" value={form.email} onChange={(e) => setForm(prev => ({ ...prev, email: e.target.value })) } required />
+      <input type="email" className=" border-1 border-primary-border rounded-xl px-3 py-3" placeholder="Email" value={form.email} onChange={(e) => setForm((pr) => ({ ...pr, email: e.target.value }))} required />
       <label htmlFor="">password</label>
-      <input type="password" className=" border-1 border-primary-border rounded-xl  px-3 py-3" placeholder="Password" value={form.password} onChange={(e) => setForm(prev => ({ ...prev, password: e.target.value }))} required />
+      <input type="password" className=" border-1 border-primary-border rounded-xl  px-3 py-3" placeholder="Password" value={form.password} onChange={(e) => setForm((prev) => ({ ...prev, password: e.target.value }))} required />
 
       <button type="submit" className="bg-primary text-white py-3 rounded-xl mt-8 mb-3 ">
         Login
